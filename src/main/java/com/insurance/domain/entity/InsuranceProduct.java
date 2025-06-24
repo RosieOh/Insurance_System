8e0af3c4d298f8c31db161b291cecff10924ff7d
RosieOh <dhxogns920@gmail.com>
2025-06-25 00:40:32 +0900

    FEAT : 보험 상품 엔티티 객체 추가

---
+++ src/main/java/com/insurance/domain/entity/InsuranceProduct.java
@@ -0,0 +1,39 @@
+package com.insurance.domain;
+
+import com.core.common.audit.AuditEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigDecimal;
+
+/**
+ * 보험상품 엔티티
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "insurance_product")
+public class InsuranceProduct extends AuditEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal premium;
+
+    @Column(length = 200)
+    private String coverage;
+} 
