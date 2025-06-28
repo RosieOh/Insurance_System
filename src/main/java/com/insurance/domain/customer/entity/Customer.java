75c41399943f95d377d43577fb78630a58469381
RosieOh <dhxogns920@gmail.com>
2025-06-28 18:10:23 +0900

    FEAT : 고객 엔티티 추가

---
+++ src/main/java/com/insurance/domain/customer/entity/Customer.java
@@ -0,0 +1,37 @@
+package com.insurance.domain;
+
+import com.core.common.audit.AuditEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+/**
+ * 고객 엔티티
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "customer")
+public class Customer extends AuditEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String email;
+
+    @Column(length = 20)
+    private String phone;
+
+    @Column(length = 200)
+    private String address;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Contract> contracts;
+} 
