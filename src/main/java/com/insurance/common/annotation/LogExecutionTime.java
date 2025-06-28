6ab59f1a88f38f18a9cd759bf1e4c71023f82b5f
RosieOh <dhxogns920@gmail.com>
2025-06-28 19:10:59 +0900

    CHORE : 기존 데이터 베이스를 인메모리 데이터 베이스로 변경

--- src/main/java/com/insurance/common/annotation/LogExecutionTime.java
+++ src/main/java/com/insurance/common/annotation/LogExecutionTime.java
@@ -1,12 +1,14 @@
-package com.core.common.annotation;
+package com.insurance.common.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 메서드 실행 시간을 측정하기 위한 어노테이션
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface LogExecutionTime {
-    String value() default "";
 } 
